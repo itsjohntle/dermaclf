b1beda2ebaf232d88d4ef13b8923f46b74a6cd73
John Le <jjuniorin@gmail.com>
2026-07-13 14:58:56 -0700

    fix(split_data): errors in parameters and return

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,6 +1,4 @@
 from sklearn.model_selection import train_test_split
-import numpy as np
-import pandas as pd
 from ucimlrepo import fetch_ucirepo
 
 def load_dermatology_data():
@@ -16,17 +14,16 @@ def load_dermatology_data():
 
     return X, y
 
-def split_data(X, y, test_size=0.2, stratify=y, random_state=42):
+def split_data(X, y, test_size=0.2, random_state=42):
     """Splits features and targets into training and test sets.
 
     Args:
         X (pandas.DataFrame): Feature matrix.
         y (pandas.DataFrame): Target labels.
         test_size (float): Proportion of the dataset to include in the test split.
-        straify (array_like): Labels used to straify the split.
-        random-state (int): Seed for reproducibile shuffling/splitting
+        random_state (int): Seed for reproducibile shuffling/splitting
 
     Returns:
-        list: [X_train, X_test, y_train, y_test]
+        tuple: (X_train, X_test, y_train, y_test)
     """
-    return train_test_split(X, y, test_size, stratify, random_state)
+    return train_test_split(X, y, test_size=test_size, stratify=y, random_state=random_state)
