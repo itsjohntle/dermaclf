bac870e9de3915c55942bb216ba4ce9c99e2ae60
John Le <jjuniorin@gmail.com>
2026-07-13 02:12:01 -0700

    feat(split_data): splits features/targets into training and test sets

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -16,4 +16,17 @@ def load_dermatology_data():
 
     return X, y
 
+def split_data(X, y, test_size=0.2, stratify=y, random_state=42):
+    """Splits features and targets into training and test sets.
 
+    Args:
+        X (pandas.DataFrame): Feature matrix.
+        y (pandas.DataFrame): Target labels.
+        test_size (float): Proportion of the dataset to include in the test split.
+        straify (array_like): Labels used to straify the split.
+        random-state (int): Seed for reproducibile shuffling/splitting
+
+    Returns:
+        list: [X_train, X_test, y_train, y_test]
+    """
+    return train_test_split(X, y, test_size, stratify, random_state)
